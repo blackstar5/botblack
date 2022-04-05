0bec9a98be75bb308809861aae99e490cdafa1fc
blackstar5 <101946567+blackstar5@users.noreply.github.com>
2022-04-05 14:36:04 +0430

    Update hosyn.py

--- hosyn.py
+++ hosyn.py
@@ -699,7 +699,7 @@ def get_backup(text,chat,bot):
 
 g_usvl = ''
 test_usvl = ''
-auth = "hqmpzihkcnpkqqipiowxjnffvvlyondk"
+auth = "wgsmzeyujivopgwrnacnimxahkwcjsoo"
 bot = Bot(auth)
 list_message_seened = []
 time_reset = random._floor(datetime.datetime.today().timestamp()) + 350
